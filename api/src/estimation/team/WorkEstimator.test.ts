6f12b01394ea84b08543b0bc913b0659a0db417a
Christian Abegg <Christian.Abegg@zuehlke.com>
2023-03-08 10:25:04 +0100

    fix work estimator test

--- api/src/estimation/team/WorkEstimator.test.ts
+++ api/src/estimation/team/WorkEstimator.test.ts
@@ -1,29 +1,30 @@
 import {describe, expect, test} from "vitest";
 import {testTeam} from "../../testing/testTeam";
 import {WorkEstimator} from "./WorkEstimator";
+import {BUSINESS_DAYS_PER_YEAR} from "../common/Constants";
 
 describe("WorkEstimator", () => {
 
-    const baseExpectation_kWh = testTeam.workingDays_perYear * testTeam.duration_years * (testTeam.teamDistribution_nr.mainLocation + testTeam.teamDistribution_nr.remoteLocation) * testTeam.workingHours_perDay * testTeam.workplacePowerUsage_W / 1000;
+    const baseExpectation_kWh = ((testTeam.workplacePowerUsage_W * BUSINESS_DAYS_PER_YEAR * testTeam.duration_years * testTeam.workingHours_perDay) / 1000);
 
     test("estimates main location impact", () => {
         const impact = new WorkEstimator().estimate({
             ...testTeam,
-            teamDistribution_nr: {...testTeam.teamDistribution_nr, mainLocation: 0.6},
+            teamDistribution_nr: {...testTeam.teamDistribution_nr, mainLocation: 6},
             emissionFactor_gC02eqPerKWh: {...testTeam.emissionFactor_gC02eqPerKWh, mainLocation: 5}
         });
         const actual = impact.get("mainLocation");
-        expect(actual!.gC02eq).toBe(baseExpectation_kWh * 0.6 * 5);
+        expect(actual!.gC02eq).toBe(baseExpectation_kWh * 6 * 5);
     });
 
     test("estimates remote location impact", () => {
         const impact = new WorkEstimator().estimate({
             ...testTeam,
-            teamDistribution_nr: {...testTeam.teamDistribution_nr, remoteLocation: 0.7},
+            teamDistribution_nr: {...testTeam.teamDistribution_nr, remoteLocation: 7},
             emissionFactor_gC02eqPerKWh: {...testTeam.emissionFactor_gC02eqPerKWh, remoteLocation: 4}
         });
         const actual = impact.get("remoteLocation");
-        expect(actual!.gC02eq).toBe(baseExpectation_kWh * 0.7 * 4);
+        expect(actual!.gC02eq).toBe(baseExpectation_kWh * 7 * 4);
     });
 });
 
--- api/src/estimation/team/WorkEstimator.ts
+++ api/src/estimation/team/WorkEstimator.ts
@@ -1,6 +1,7 @@
 import {ITeam, LocationTypes} from "./ITeam";
 import {Impact} from "../Impact";
 import {totalWorkingDays} from "./totalWorkingDays";
+import {BUSINESS_DAYS_PER_YEAR} from "../common/Constants";
 
 export class WorkEstimator {
 
@@ -12,9 +13,8 @@ export class WorkEstimator {
     }
 
     private estimateWork(team: ITeam, location: keyof LocationTypes): [keyof LocationTypes, Impact] {
-        const days = totalWorkingDays(team)
-        const gC02eq = ((team.workplacePowerUsage_W * days * team.workingHours_perDay) / 1000) *  team.teamDistribution_nr[location] * team.emissionFactor_gC02eqPerKWh[location] / 1000;
-        const formula = `((${team.workplacePowerUsage_W}W * ${days}d * ${team.workingHours_perDay}h) / 1000 ) * ${team.teamDistribution_nr[location]} [persons] * ${team.emissionFactor_gC02eqPerKWh[location]}gC02eq per kWh`;
+        const gC02eq = ((team.workplacePowerUsage_W  * BUSINESS_DAYS_PER_YEAR * team.duration_years * team.workingHours_perDay) / 1000) *  team.teamDistribution_nr[location] * team.emissionFactor_gC02eqPerKWh[location];
+        const formula = `((${team.workplacePowerUsage_W}W * ${BUSINESS_DAYS_PER_YEAR}d/y * ${team.duration_years}y * ${team.workingHours_perDay}h) / 1000 ) *  ${team.teamDistribution_nr[location]} [persons] * ${team.emissionFactor_gC02eqPerKWh[location]}gC02eq per kWh`;
         return [location, new Impact(gC02eq, formula)];
     }
 }
