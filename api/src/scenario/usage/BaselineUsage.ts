609cfd115432ecff34fc044ecc7934f3b70d73e1
Christian Abegg <Christian.Abegg@zuehlke.com>
2023-03-08 13:17:10 +0100

    correct internetTraffic

--- api/src/scenario/usage/BaselineUsage.ts
+++ api/src/scenario/usage/BaselineUsage.ts
@@ -7,11 +7,11 @@ export class BaselineUsage implements IUsage {
     private developmentTeam = new DevelopmentTeam();
     private maintenanceTeam = new MaintenanceTeam();
 
-    additionalRequest_gb = 0.001; // 100kb
+    additionalRequest_gb = 0.0001; // 100kb
     duration_years = this.maintenanceTeam.duration_years;
     workplaceEmbodiedEmissions_gC02eq = this.developmentTeam.workplaceEmbodiedEmissions_gCO2eq;
     workplaceExpectedLifespan_years = this.developmentTeam.workplaceExpectedLifespan_years;
-    initialRequest_gb = 0.035; // 3.5mb
+    initialRequest_gb = 0.0035; // 3.5mb
     trafficCoefficient_kWhPerGb = 0.06;  // Source: https://www.cloudcarbonfootprint.org/docs/methodology/#appendix-iv-recent-networking-studies
     trafficEmissionFactor_gC02eqPerKWh = 250; // Source: https://app.electricitymaps.com/map for 2022, averaged over multiple European countries
     usagePerUserAndBusinessDay_h = 1; // application is used 1 hour
