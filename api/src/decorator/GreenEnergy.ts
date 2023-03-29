a77e122af78cac22c8f5ebb1e3f1ea641942b7c8
Christian Abegg <Christian.Abegg@zuehlke.com>
2023-03-29 10:30:13 +0200

    adjust emission factors

--- api/src/decorator/GreenEnergy.ts
+++ api/src/decorator/GreenEnergy.ts
@@ -8,14 +8,12 @@ import {IUsage} from "../interfaces/IUsage";
  */
 export class GreenEnergy extends ProjectDecorator {
 
-    // ewz.pronatur 20221 emission factor 19 gCO2eq/kWh
-    private static EMISSIONS_FACTOR = 19;
-
     protected override decorateTeam(team: ITeam): ITeam {
         return Object.assign(team, {
+            // ewz.pronatur 20221 emission factor 19 gCO2eq/kWh
             emissionFactor_gC02eqPerKWh: {
-                mainLocation:   Math.min(GreenEnergy.EMISSIONS_FACTOR, team.emissionFactor_gC02eqPerKWh.mainLocation),
-                remoteLocation: Math.min(GreenEnergy.EMISSIONS_FACTOR, team.emissionFactor_gC02eqPerKWh.remoteLocation),
+                mainLocation:   Math.min(19, team.emissionFactor_gC02eqPerKWh.mainLocation),
+                remoteLocation: Math.min(19, team.emissionFactor_gC02eqPerKWh.remoteLocation),
             },
             // Source: https://www.mobitool.ch/de/tools/mobitool-faktoren-v2-1-25.html?tag=3
             videoconference_gC02eqPerHour: 40
@@ -24,13 +22,21 @@ export class GreenEnergy extends ProjectDecorator {
 
     protected override decorateMachine(machine: IMachine): IMachine {
         return Object.assign(machine, {
-            emissionFactor_gC02eqPerkWh: Math.min(GreenEnergy.EMISSIONS_FACTOR, machine.emissionFactor_gC02eqPerkWh)
+            // Azure emission factor is unclear/unrealistic:
+            // - 5.67 g/kWh according to https://www.cloudcarbonfootprint.org/docs/methodology#appendix-i-energy-coefficients
+            // - 11.52 g/kWh according to https://www.carbonfootprint.com/docs/2022_03_emissions_factors_sources_for_2021_electricity_v11.pdf (country specific electricity factor)
+            // - but ewz.pronatur 20221 emission factor 19 gCO2eq/kWh - and that is already considered low
+            // AWS and GCP are a bit more realistic:
+            // - AWS Germany: 311 g/kWh according to https://www.cloudcarbonfootprint.org/docs/methodology#appendix-i-energy-coefficients
+            // - GCP europe-west6 (CH): 86 g/kWh according to https://cloud.google.com/sustainability/region-carbon
+            emissionFactor_gC02eqPerkWh: Math.min(86, machine.emissionFactor_gC02eqPerkWh)
         });
     }
 
     protected override decorateUsage(usage: IUsage): IUsage {
         return Object.assign(usage, {
-            workplaceEmissionFactor_gC02eqPerKWh: Math.min(GreenEnergy.EMISSIONS_FACTOR, usage.workplaceEmissionFactor_gC02eqPerKWh)
+            // ewz.pronatur 20221 emission factor 19 gCO2eq/kWh
+            workplaceEmissionFactor_gC02eqPerKWh: Math.min(19, usage.workplaceEmissionFactor_gC02eqPerKWh)
         });
     }
 }
